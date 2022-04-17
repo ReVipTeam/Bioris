27174917a4a137dd343d99f2b63c9a004255d0f2
Efim <efim-sys@yandex.ru>
2022-04-17 12:22:31 +0300

    spawn

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const mapDiv = document.getElementById('mapDiv');
 const point = document.getElementById('point');
-mapDiv.addEventListener("mousemove", function(e) {
-  
-  point.style.setProperty('left', (''+e.clientX)+"px");
-  point.style.setProperty('top', (''+e.clientY)+"px");
+
+function newArea(x, y) {
+  //console.log("<div class='point' " + "style='left: "+x+"; top:"+y+";'></div>");
+  mapDiv.innerHTML+="<div class='point' " + "style='left: "+x+"px; top:"+y+"px; position: absolute;'></div>"
+}
+
+
+mapDiv.addEventListener("click", function(e) {
+  newArea(e.clientX, e.clientY);
+  //point.style.setProperty('left', (''+e.clientX)+"px");
+  //point.style.setProperty('top', (''+e.clientY)+"px");
 })
