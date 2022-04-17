7234b00435475311c23a163f401e45a78a610968
Efim <efim-sys@yandex.ru>
2022-04-17 12:03:04 +0300

    no console.log

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const mapDiv = document.getElementById('mapDiv');
 const point = document.getElementById('point');
 mapDiv.addEventListener("mousemove", function(e) {
-  console.log("move")
+  
   point.style.setProperty('left', (''+e.clientX)+"px");
   point.style.setProperty('top', (''+e.clientY)+"px");
 })
