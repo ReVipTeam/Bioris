cf6affee5198ec314d34f7fdf664949fe2c890d0
Efim <efim-sys@yandex.ru>
2022-04-17 14:22:18 +0300

    рисовашка

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ function newArea(x, y, r, c) {
   mapDiv.innerHTML+="<div class='point' style='left: "+x+"px; top:"+y+"px; background-color: rgba("+255+", 0, 0, "+c+"); box-shadow: 0 0 "+r+"px "+(r-20)+"px rgba("+255+", 0, 0, "+c+");'></div>"
 }
 
+function newColorArea(x, y, r, c) {
+  mapDiv.innerHTML+="<div class='point' style='left: "+x+"px; top:"+y+"px; background-color: "+c+"; box-shadow: 0 0 "+r+"px "+(r-20)+"px "+c+";'></div>"
+}
 
-mapDiv.addEventListener("click", function(e) {
-  newArea(e.clientX, e.clientY, Math.random()*100, Math.random());
+let color = "#ff0000"
+
+let mouseDown = false
+
+let size = 50;
+
+mapDiv.addEventListener("mousedown", function(e) {mouseDown=true});
+mapDiv.addEventListener("mouseup", function(e) {mouseDown=false});
+
+mapDiv.addEventListener("mousemove", function(e) {
+  if(mouseDown) newColorArea(e.clientX, e.clientY, size, color);
+  //point.style.setProperty('left', (''+e.clientX)+"px");
+  //point.style.setProperty('top', (''+e.clientY)+"px");
+})
+
+colorPicker.addEventListener("change", function() {
+  color = colorPicker.value;
   //point.style.setProperty('left', (''+e.clientX)+"px");
   //point.style.setProperty('top', (''+e.clientY)+"px");
 })
