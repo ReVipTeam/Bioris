7c47688c3ff6c058488980ec963b3c72947d69ae
Efim <efim-sys@yandex.ru>
2022-04-17 13:18:56 +0300

    fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const mapDiv = document.getElementById('mapDiv');
 const point = document.getElementById('point');
 
-function newArea(x, y, c) {
+function newArea(x, y, r, c) {
   //console.log("<div class='point' " + "style='left: "+x+"; top:"+y+";'></div>");
   mapDiv.innerHTML+="<div class='point' style='left: "+x+"px; top:"+y+"px; background-color: rgb("+c*255+", 0, 0); box-shadow: 0 0 "+r+"px "+(r-20)+"px rgb("+c*255+", 0, 0);'></div>"
 
@@ -9,7 +9,7 @@ function newArea(x, y, c) {
 
 
 mapDiv.addEventListener("click", function(e) {
-  newArea(e.clientX, e.clientY, Math.random());
+  newArea(e.clientX, e.clientY, Math.random()*200, Math.random());
   //point.style.setProperty('left', (''+e.clientX)+"px");
   //point.style.setProperty('top', (''+e.clientY)+"px");
 })
